984a4719c29723acf9b7e3c18b2c07ea1d352828
Jeff Jiang <jeff.jiang.cd.cn@gmail.com>
2022-10-06 15:59:40 +0800

    bugfix

--- server/server.go
+++ server/server.go
@@ -1,9 +1,21 @@
 package server
 
-import "github.com/gin-gonic/gin"
+import (
+	"errors"
+
+	"github.com/gin-gonic/gin"
+)
+
+var errServerEngineNotInit = errors.New("server engine was not initialized, please call server.NewServer() to  initialize server")
 
 type Server struct {
 	engine *gin.Engine
+}
+
+func NewServer() *Server {
+	return &Server{
+		engine: gin.New(),
+	}
 }
 
 func (server *Server) Engine() *gin.Engine {
@@ -11,6 +23,10 @@ func (server *Server) Engine() *gin.Engine {
 }
 
 func (server *Server) Start(addr ...string) error {
+
+	if server.engine == nil {
+		return errServerEngineNotInit
+	}
 
 	err := server.engine.SetTrustedProxies(nil)
 	if err != nil {
@@ -28,6 +44,9 @@ func (server *Server) Stop() error {
 }
 
 func (server *Server) WithOption(opts ...Option) (*Server, error) {
+	if server.engine == nil {
+		return nil, errServerEngineNotInit
+	}
 	for _, opt := range opts {
 		err := opt.Apply(server)
 		if err != nil {
