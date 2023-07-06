6edabc75d27c2ca4d0793848450088e76f0a63d8
jeffJiang <45201325+anyufly@users.noreply.github.com>
2023-07-06 12:58:36 +0800

    Update context_key.go

--- common/context_key.go
+++ common/context_key.go
@@ -1,15 +1,4 @@
+
 package common
 
-import (
-	"github.com/gin-gonic/gin"
-)
-
-func GetRouterPath(ctx *gin.Context) string {
-	reqCtx := ctx.Request.Context()
-	value := reqCtx.Value(ContextKey("router_path"))
-	routerPath, ok := value.(string)
-	if !ok {
-		return ""
-	}
-	return routerPath
-}
+type ContextKey string
