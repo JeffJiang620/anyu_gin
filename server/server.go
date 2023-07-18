33b17b1bdb50d364f9a4586f6c1776f1516020eb
jeff <jeff.jiang.cd.cn@gmail.com>
2023-07-18 11:54:12 +0800

    graceful shutdown

--- server/server.go
+++ server/server.go
@@ -1,7 +1,10 @@
 package server
 
 import (
+	"context"
 	"errors"
+	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,6 +13,7 @@ var errServerEngineNotInit = errors.New("server engine was not initialized, plea
 
 type Server struct {
 	engine *gin.Engine
+	srv    *http.Server
 }
 
 func NewServer(mode string) *Server {
@@ -23,6 +27,20 @@ func (server *Server) Engine() *gin.Engine {
 	return server.engine
 }
 
+func resolveAddress(addr []string) string {
+	switch len(addr) {
+	case 0:
+		if port := os.Getenv("PORT"); port != "" {
+			return ":" + port
+		}
+		return ":8080"
+	case 1:
+		return addr[0]
+	default:
+		panic("too many parameters")
+	}
+}
+
 func (server *Server) Start(addr ...string) error {
 
 	if server.engine == nil {
@@ -33,14 +51,28 @@ func (server *Server) Start(addr ...string) error {
 	if err != nil {
 		return err
 	}
-	err = server.engine.Run(addr...)
+
+	address := resolveAddress(addr)
+
+	server.srv = &http.Server{
+		Addr:    address,
+		Handler: server.engine,
+	}
+
+	err = server.srv.ListenAndServe()
+
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func (server *Server) Stop() {}
+func (server *Server) Stop(ctx context.Context) error {
+	if server.srv == nil {
+		return nil
+	}
+	return server.srv.Shutdown(ctx)
+}
 
 func (server *Server) WithOption(opts ...Option) (*Server, error) {
 	if server.engine == nil {
